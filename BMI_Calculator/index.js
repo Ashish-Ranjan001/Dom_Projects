b3e782613afcbfa2136e5f1ffa981f5afe4bb2d7
Ashish-Ranjan001 <106318991+Ashish-Ranjan001@users.noreply.github.com>
2024-06-05 01:40:30 +0530

    External msg On Submit

--- BMI_Calculator/index.js
+++ BMI_Calculator/index.js
@@ -15,7 +15,19 @@ result.innerHTML = 'Please give a valid weight'
 }
 else{
     const bmi = (weight / ((height*height)/10000)).toFixed(2);
-    result.innerHTML=`<span>${bmi}</span>`
+    result.innerHTML=`<span>${bmi}</span>`;
+    const msg = document.querySelector('.msg')
+
+    if(bmi<=18.6){
+        msg.innerHTML=`You Falls in Under Weight Category, Your bmi should be more than ${bmi}`
+        }
+        if(bmi>=18.6 && bmi<=24.9){
+        msg.innerHTML=`You Falls in Normal Range Category, Your bmi is Perfect`
+        }
+        if(bmi>24.9){
+        msg.innerHTML=`You Falls in OverWeight Category, Your bmi should be less than ${bmi}`
+        }
 }
 
 })
+
